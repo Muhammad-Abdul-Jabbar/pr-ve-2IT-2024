53db6455e9e64d6f48dfdfa402ea7e78296c5cf2
Abdul Jabbar <122173146+Muhammad-Abdul-Jabbar@users.noreply.github.com>
2024-03-12 12:10:42 -0700

    css.javascript.html

---
+++ JavaScript/login.js
@@ -0,0 +1,45 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+  import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBUxSe6guEi8HsQ1mTmx5Cka41R6LXVUKw",
+    authDomain: "tentamen-pr.firebaseapp.com",
+    projectId: "tentamen-pr",
+    storageBucket: "tentamen-pr.appspot.com",
+    messagingSenderId: "506204527398",
+    appId: "1:506204527398:web:cbefb3fd40aad41bcfffe7"
+  };
+
+  // Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth()
+ 
+var email = document.getElementById("email");
+var password = document.getElementById("password");
+window.login= function(e) {
+  e.preventDefault();
+  var obj = {
+    email: email.value,
+    password: password.value,
+  };
+
+  signInWithEmailAndPassword(auth, obj.email, obj.password)
+    .then(function (success) {
+      alert("logined Successfully")
+      var aaaa =  (success.user.uid);
+      localStorage.setItem("uid",aaaa)
+      console.log(aaaa)
+      window.location.href = "UserPage.html";
+     // localStorage.setItem(success,user,uid)
+      
+    })
+    .catch(function (err) {
+      alert("login error"+err);
+    });
+
+  console.log(obj);
+}
---
+++ JavaScript/reg.js
@@ -0,0 +1,47 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+  import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBUxSe6guEi8HsQ1mTmx5Cka41R6LXVUKw",
+    authDomain: "tentamen-pr.firebaseapp.com",
+    projectId: "tentamen-pr",
+    storageBucket: "tentamen-pr.appspot.com",
+    messagingSenderId: "506204527398",
+    appId: "1:506204527398:web:cbefb3fd40aad41bcfffe7"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+const auth = getAuth()
+
+var firstname = document.getElementById("firstname")
+var lastname = document.getElementById("lastname")
+var email = document.getElementById("Email")
+var password = document.getElementById("password")
+
+window.signup = function(e){
+   e.preventDefault();
+   var obj = {
+       firstname:firstname.value,
+       lastname:lastname.value,
+       email:email.value,
+       password:password.value,
+   }
+   createUserWithEmailAndPassword(auth, obj.email,obj.password)
+   .then(function(success){
+       alert("signup successfully")
+       window.location.href = "UserPage.html";
+   })
+   .catch(function(err){
+       alert("error" + err)
+   })
+   console.log(obj)
+};
+
+
